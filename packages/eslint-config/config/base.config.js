c9111db91232decfb8359049b10d45eab6c2236f
CodeDragon03 <jay7666.professional@gmail.com>
2025-08-31 22:42:09 +0530

    Initial Setup

---
+++ packages/eslint-config/config/base.config.js
@@ -0,0 +1,42 @@
+import js from '@eslint/js';
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import eslintTypescriptParser from '@typescript-eslint/parser';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import turboConfig from 'eslint-config-turbo/flat';
+import turboEslintPlugin from 'eslint-plugin-turbo';
+
+/**
+ * A shared ESLint configuration for the repository.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ * */
+
+export default [
+  ...turboConfig,
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    parser: eslintTypescriptParser,
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+  },
+
+  {
+    plugins: {
+      turbo: turboEslintPlugin,
+      '@typescript-eslint': typescriptEslintPlugin,
+    },
+    extends: ['plugin:@typescript-eslint/recommended'],
+    rules: {
+      'import/order': 'off',
+      'turbo/no-undeclared-env-vars': 'warn',
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
+  },
+
+  {
+    ignores: ['dist/**'],
+  },
+];
---
+++ prettier.config.js
@@ -0,0 +1,24 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+
+const config = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
+
+export default config;
